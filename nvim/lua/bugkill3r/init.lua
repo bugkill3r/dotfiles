960365f02b442a6f46cf1e8d516fcda715edff53
Saurabh <skmishra1991@gmail.com>
2022-10-30 05:02:06 +0530

    added nvim -- draft

---
+++ nvim/lua/bugkill3r/init.lua
@@ -0,0 +1,42 @@
+require("bugkill3r.set")
+require("bugkill3r.remap")
+require("bugkill3r.packer")
+
+local augroup = vim.api.nvim_create_augroup
+bugkill3rGroup = augroup('bugkill3r', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = bugkill3rGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = bugkill3rGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
